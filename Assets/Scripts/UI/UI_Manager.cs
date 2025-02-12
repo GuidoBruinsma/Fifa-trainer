9243abee0a608cd698083172ae6c2705ecfe7a07
Stanislav <taix97@outlook.com>
2025-02-12 16:19:26 +0100

    Improves
    UI manager, sequence visualizer, Added next move prediction, added all buttons, new control map settings

--- Assets/Scripts/UI/UI_Manager.cs
+++ Assets/Scripts/UI/UI_Manager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -5,8 +6,23 @@ public class UI_Manager : MonoBehaviour
 {
     public static UI_Manager Instance { get; private set; }
 
+    [Header("Settings")]
+    [SerializeField] private SkillControlIconMap skillMap;
+
+    [Space]
+
+    [Header("Time Related Section")]
     [SerializeField] private TextMeshProUGUI timeleftText;
     [SerializeField] private TextMeshProUGUI timeElapsedText;
+    [SerializeField] private TextMeshProUGUI timeElapsedComlpetionText;
+
+    [Space]
+
+    [Header("Next Move Section")]
+    [SerializeField] private TextMeshProUGUI nextMoveName;
+    [SerializeField] private TextMeshProUGUI nextMoveSequence;
+
+    [SerializeField] private TextMeshProUGUI sequenceText;
 
     private void Awake()
     {
@@ -16,11 +32,27 @@ public class UI_Manager : MonoBehaviour
             Destroy(this.gameObject);
     }
 
-    public void SetTimerText(float timeLeft) {
+    public void SetTimerText(float timeLeft)
+    {
         timeleftText.text = timeLeft.ToString("0.0");
     }
-    
-    public void SetElapsedTimeText(float elapsedTime) {
-        timeElapsedText.text = elapsedTime.ToString("00:00");
+
+    public void SetElapsedTimeText(float elapsedTime)
+    {
+        int minutes = Mathf.FloorToInt(elapsedTime / 60f);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60f);
+        timeElapsedText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
+
+    public void SetElapsedTimeCompletion(float elapsedTime) => timeElapsedComlpetionText.text = $"Skill completion time: {elapsedTime.ToString("0.000")}";
+
+    public void SetSkillMoveInfo(string sequence) => sequenceText.text = sequence;
+
+    public void SetNextMoveInfo(List<SkillInput> sequenceList, string moveName)
+    {
+        nextMoveName.text = moveName;
+        nextMoveSequence.text = skillMap.GetSequenceDisplay(sequenceList);
+    }
+
+
 }
