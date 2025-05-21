97b305163133318a6c3df02c7ce140d11f406562
Stan <taix97@outlook.com>
2025-05-21 15:24:24 +0200

    started working on 'all time analyze' panel

---
+++ Assets/Scripts/Data/SkillScrollViewData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class SkillScrollViewData : MonoBehaviour
+{
+    //TODO: Get data from saved json and create the buttons and add it to the scroll view
+    //when selected, send the data of that one to the chart and the rest of the windows that shows whatever information
+}
